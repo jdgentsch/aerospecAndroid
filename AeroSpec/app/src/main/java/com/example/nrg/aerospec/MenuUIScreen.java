744c64b73af29643728bcffe3728e3f11e0d13de
jj-fly <john.redfield04@gmail.com>
2016-08-09 17:36:25 -0700

    second ui screen added

---
+++ AeroSpec/app/src/main/java/com/example/nrg/aerospec/MenuUIScreen.java
@@ -0,0 +1,13 @@
+package com.example.nrg.aerospec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuUIScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_uiscreen);
+    }
+}
