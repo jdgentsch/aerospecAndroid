8d543a1b5ca75f6e1920a08f88545c2244df1410
jj-fly <john.redfield04@gmail.com>
2016-08-11 00:23:11 -0700

    fourth edit with fonts.md

--- AeroSpec/app/src/main/java/com/example/nrg/aerospec/MainUIScreen.java
+++ AeroSpec/app/src/main/java/com/example/nrg/aerospec/MainUIScreen.java
@@ -1,7 +1,13 @@
 package com.example.nrg.aerospec;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainUIScreen extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainUIScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_uiscreen);
+        Typeface emailTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-Hairline.otf");
+        TextView emailTextview = (EditText)findViewById(R.id.editText);
+        emailTextview.setTypeface(emailTypeface);
+
+        Typeface fgTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-Light.otf");
+        TextView fgTextview = (TextView)findViewById(R.id.textView);
+        fgTextview.setTypeface(fgTypeface);
+
+        Typeface logTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-UltraLight.otf");
+        TextView logTextview = (Button)findViewById(R.id.button);
+        logTextview.setTypeface(logTypeface);
+
+        Typeface regTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-UltraLight.otf");
+        TextView regTextview = (Button)findViewById(R.id.button2);
+        regTextview.setTypeface(regTypeface);
     }
 }
--- AeroSpec/app/src/main/java/com/example/nrg/aerospec/StartNewScanUI.java
+++ AeroSpec/app/src/main/java/com/example/nrg/aerospec/StartNewScanUI.java
@@ -1,7 +1,9 @@
 package com.example.nrg.aerospec;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StartNewScanUI extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class StartNewScanUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startnewscan_ui);
+        Typeface myTypeFace = Typeface.createFromAsset(getAssets(), "Montserrat-Bold.otf");
+        TextView myTextView = (TextView)findViewById(R.id.textView3);
+        myTextView.setTypeface(myTypeFace);
     }
 }
