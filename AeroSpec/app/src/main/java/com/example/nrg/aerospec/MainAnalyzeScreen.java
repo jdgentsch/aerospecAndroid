d9556c11da57b33fa5a9a24e6672ba45b3efebae
al3xj33 <alexjee@uw.edu>
2016-08-14 22:19:33 -0700

    Added the Analyze screen, started a new screen for Filter

---
+++ AeroSpec/app/src/main/java/com/example/nrg/aerospec/MainAnalyzeScreen.java
@@ -0,0 +1,32 @@
+package com.example.nrg.aerospec;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainAnalyzeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_analyze);
+        Typeface emailTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-Hairline.otf");
+        TextView emailTextview = (EditText)findViewById(R.id.editText);
+        emailTextview.setTypeface(emailTypeface);
+
+        Typeface fgTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-Light.otf");
+        TextView fgTextview = (TextView)findViewById(R.id.textView);
+        fgTextview.setTypeface(fgTypeface);
+
+        Typeface logTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-UltraLight.otf");
+        TextView logTextview = (Button)findViewById(R.id.button);
+        logTextview.setTypeface(logTypeface);
+
+        Typeface regTypeface = Typeface.createFromAsset(getAssets(), "Montserrat-UltraLight.otf");
+        TextView regTextview = (Button)findViewById(R.id.button2);
+        regTextview.setTypeface(regTypeface);
+    }
+}
